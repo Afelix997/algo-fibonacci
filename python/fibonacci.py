7974c8e9e5f1d5f9256811f2d8d32310f4552873
afelix997 <afelix2013@gmail.com>
2022-05-25 15:01:22 -0700

    new

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,14 @@
-def fibonacci(n):
-  pass
+def fibonacci(num):
+  num1=0
+  num2=1
+  sum=0
+  
+  for i in range(0,num):
+    sum = num1 +num2
+    num1 = num2
+    num2 = sum
+  return num1      
+
+
 
 
